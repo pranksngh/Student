3747789cc61594bf9377e265fb72ad2c6b408214
Prashant <pranksngh@gmail.com>
2024-06-24 15:24:35 +0530

    new update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,19 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+export default function Layout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
-  );
+    <Stack initialRouteName="login">
+     <Stack.Screen name="index"  options={{ headerShown: false }} />   
+     <Stack.Screen name="Screens/register"  options={{ headerShown: false }} /> 
+     <Stack.Screen name="Screens/courseDetails"  options={{ headerShown: false }} /> 
+     <Stack.Screen name="Screens/lessons"  options={{ headerShown: false }} /> 
+     <Stack.Screen name="Screens/assignments"  options={{ headerShown: false }} />
+     <Stack.Screen name="Screens/myclasses"  options={{ headerShown: false }} />
+     <Stack.Screen name="Screens/MyDoubts"  options={{ headerShown: false }} />
+     <Stack.Screen name="Screens/chats"  options={{ headerShown: false }} />
+     <Stack.Screen name="Screens/chatDetails"  options={{ headerShown: false }} />
+     <Stack.Screen name="Screens/Reports"  options={{ headerShown: false }} />
+     <Stack.Screen name="(drawer)"  options={{ headerShown: false }} /> 
+     
+       
+    </Stack> );
 }
